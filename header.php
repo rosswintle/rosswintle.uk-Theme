957e2fbd6d06f70c8f842cd82ab89176c45a0117
Ross Wintle <rosswintle@users.noreply.github.com>
2023-10-28 21:18:40 +0100

    Add basic view transitions for those that have them

--- header.php
+++ header.php
@@ -16,6 +16,7 @@
 	<meta charset="<?php bloginfo( 'charset' ); ?>">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
 	<link rel="profile" href="http://gmpg.org/xfn/11">
+	<meta name="view-transition" content="same-origin" />
 
 	<?php wp_head(); ?>
 </head>
